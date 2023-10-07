b821f76e72b22d480027e9f38a6f178ef90f379d
idekworks <99700493+idekworks@users.noreply.github.com>
2023-10-07 12:44:04 +0900

    simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-ally",
   ],
   framework: {
     name: "@storybook/react-vite",
